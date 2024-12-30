152847035e52c60bea13fd5c5e7757723c1081cd
Onin <jonielbolocon51@gmail.com>
2024-12-30 23:34:17 +0800

    added content for the other levels and saved the current state of music and welcome message

--- js/main.js
+++ js/main.js
@@ -4,36 +4,55 @@ const enterPhrase = document.querySelector('.press-enter')
 const menuMusic = document.getElementById('bgMusic')
 menuMusic.load();
 
+
+const hasVisited = localStorage.getItem('hasVisitedGame');
 let musicHasStarted = false;
+let musicIsMuted = localStorage.getItem('musicMuted') === 'true';
+
+
+if (hasVisited) {
+    overlay.classList.add('hidden');
+    overlayContent.classList.add('hidden');
+    enterPhrase.classList.add('hidden');
+    menuMusic.play();
+    musicHasStarted = true;
+}
+
+if (musicIsMuted) {
+    menuMusic.pause();
+    document.querySelector('.sound-icon').classList.add('grayed');
+}
 
 const startInteraction = () => {
-    if(!musicHasStarted){
+    if (!musicHasStarted && !hasVisited) {
         menuMusic.play();
         overlay.classList.add('hidden');
         overlayContent.classList.add('hidden');
-        enterPhrase.classList.add('hidden')
+        enterPhrase.classList.add('hidden');
         musicHasStarted = true;
-        document.removeEventListener('click' , startInteraction)
+        localStorage.setItem('hasVisitedGame', 'true');
+        document.removeEventListener('click', startInteraction);
     }
 }
 
 document.addEventListener('click', startInteraction);
 
-
 const musicIcon = document.querySelector('.sound-icon')
-let music = true;
+let music = !musicIsMuted;
 
 musicIcon.addEventListener('click', (e) => {
-    e.stopPropagation()
-    if(music){
+    e.stopPropagation();
+    if (music) {
         menuMusic.pause();
         music = false;
-        musicIcon.classList.add('grayed')
-        console.log("Music is pause")
-    }else{
+        musicIcon.classList.add('grayed');
+        localStorage.setItem('musicMuted', 'true');
+        console.log("Music is pause");
+    } else {
         menuMusic.play();
         music = true;
-        musicIcon.classList.remove('grayed')
-        console.log("Music is playing")
+        musicIcon.classList.remove('grayed');
+        localStorage.setItem('musicMuted', 'false');
+        console.log("Music is playing");
     }
 })
